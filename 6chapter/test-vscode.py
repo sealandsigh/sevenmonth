e7c9152769f2700b6c4c8adb7e99a9cd19cc98ec
sealandsigh <zhujiajunzhu@qq.com>
2019-12-15 11:18:17 +0800

    shishi

--- 6chapter/test-vscode.py
+++ 6chapter/test-vscode.py
@@ -2,9 +2,3 @@ print('hello python')
 print('hello python')
 ord('a')
 id('a')
-print('work')
-
-username = input('please input username')
-password = input('please input password')
-
-print(username,password)
