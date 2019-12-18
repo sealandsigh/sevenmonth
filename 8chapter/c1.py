2d99fdc177a874fa2a44ae92d458f98ba851bebd
sealandsigh <zhujiajunzhu@qq.com>
2019-12-18 18:27:00 +0800

    类方法

--- 8chapter/c1.py
+++ 8chapter/c1.py
@@ -4,13 +4,21 @@ class Student():
     def __init__(self,name,age):
         self.name = name
         self.age = age
-        self.__class__.sum += 1
+        #self.__class__.sum += 1
 
-        print('当前班级人数:'+ str(self.__class__.sum))
+        #print('当前班级人数:'+ str(self.__class__.sum))
 
     def do_homework(self):
         print('dohomework')
 
+    @classmethod
+    def plus_sum(cls):
+        cls.sum += 1
+        print(cls.sum)
+
 student1 = Student('zhujiajun',18)
 student2 = Student('zjj',18)
 student3 = Student('zjjj',18)
+Student.plus_sum()
+Student.plus_sum()
+Student.plus_sum()
