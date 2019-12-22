3ca7632760b2d500d4947adcbcedef1b2ef67733
sealandsigh <zhujiajunzhu@qq.com>
2019-12-22 14:06:12 +0800

    枚举类型

---
+++ 11chapter/c2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# __author__="jiajun.zhu"
+# DATE:2019/12/22
+
+from enum import Enum
+
+class VIP(Enum):
+    YELLOW = 1
+    GREEN = 2
+    RED = 3
+    BALCK =4
+
+print(VIP.YELLOW)
