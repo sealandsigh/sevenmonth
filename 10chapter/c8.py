4963ca95002b117f2920045fb55522bc7bae1461
sealandsigh <zhujiajunzhu@qq.com>
2019-12-22 09:54:20 +0800

    分组高级用法

--- 10chapter/c8.py
+++ 10chapter/c8.py
@@ -1,5 +1,12 @@
 import re
 
 a = 'life is short, i use python'
+b = 'life is short, i use python,is good python'
 r = re.search('life(.*)python',a)
-print(r.group(1))
+r1 = re.findall('life(.*)python',a)
+r2 = re.search('life(.*)python(.*)python',b)
+r3 = re.findall('life(.*)python(.*)python',b)
+# print(r.group(1))
+# print(r1)
+print(r2.group(0,1,2))
+print(r3)
